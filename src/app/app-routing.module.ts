b4f3ca9f9a1e147be91cc70840ca8ecb61f0be73
ouyangshan09 <ouyangshan09@gmail.com>
2018-08-17 16:59:11 +0800

    example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HerosComponent } from './components/heros/heros.component';
 
 const routes: Routes = [
-  { path: 'heros', component: null}
+  { path: 'heros', component: HerosComponent }
 ]
 
 @NgModule({
-  exports: [RouterModule],
-  imports: [RouterModule.forRoot(routes)]
+  exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(routes) ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
+import { HerosComponent } from './components/heros/heros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HerosComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
