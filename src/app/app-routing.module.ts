dc648ac670e17de1191047cb21b86e9a466b6441
ouyangshan09 <ouyangshan09@gmail.com>
2018-08-20 13:05:42 +0800

    添加路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RoutePath } from './constant/route.constant';
+import { HomeComponent } from './routes/home/home.component';
 
 const routes: Routes = [
+  { path: RoutePath.HOME, component: HomeComponent }
   // { path: '', redirectTo: `/${RoutePath.DASHBOARD}`, pathMatch: 'full' },
 ]
 
