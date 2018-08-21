92ef5310fd5536fefb27740b434bd7c7a98f5bbb
Ouyang <ouyangshan09@gmail.com>
2018-08-22 00:52:36 +0800

    添加auth

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+/**
+ * 用户服务
+ * 登录
+ * 退出
+ * 验证
+ * 修改密码
+ * 检查权限
+*/
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+}
